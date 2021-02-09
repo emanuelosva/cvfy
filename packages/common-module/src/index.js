294118958fe5fceb7d6098a768e5c6fb88774239
emanuelosva <emanuelosva@gmail.com>
2021-02-08 21:29:22 -0600

    chore(Feature): export getEnv function

--- packages/common-module/src/index.js
+++ packages/common-module/src/index.js
@@ -1,3 +1,9 @@
+const BuildBusinessError = require('./BussinesError')
+const makeGetEnvPrefix = require('./env')
+
+const BusinessError = new BuildBusinessError('common-module')
+
 module.exports.Logger = require('./logger')
-module.exports.BuildBusinessError = require('./BussinesError')
 module.exports.constants = require('./constants')
+module.exports.BuildBusinessError = BuildBusinessError
+module.exports.getEnvPrefix = makeGetEnvPrefix({ BusinessError })
