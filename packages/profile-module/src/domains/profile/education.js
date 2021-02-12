a23af6e162731add1cf39c3dfec035256cb82dfc
emanuelosva <emanuelosva@gmail.com>
2021-02-12 08:45:59 -0600

    chore(NewDefinition): profile domains and subdomians

---
+++ packages/profile-module/src/domains/profile/education.js
@@ -0,0 +1,31 @@
+const { MongoConnection } = require('@cvfy/mongodb-connection-module')
+const mognoose = require('mongoose')
+
+/**
+ * Schema
+ */
+const EducationSchema = new mognoose.Schema({
+  profile: { type: String, required: true },
+  title: { type: String, required: true, trim: true },
+  school: { type: String, required: true, trim: true },
+  certificateCode: { type: String, default: '' },
+  startTime: { type: Date, default: null },
+  endTime: { type: Date, default: null },
+}, {
+  id: true,
+  timestamps: true,
+  toObject: {
+    virtuals: true,
+  },
+  toJSON: {
+    virtuals: true,
+    versionKey: false,
+  },
+})
+
+/**
+ * Indexes
+ */
+EducationSchema.index({ profile: 1 })
+
+module.exports = MongoConnection().model('educations', EducationSchema)
---
+++ packages/profile-module/src/domains/profile/projects.js
@@ -0,0 +1,31 @@
+const { MongoConnection } = require('@cvfy/mongodb-connection-module')
+const mognoose = require('mongoose')
+
+/**
+ * Schema
+ */
+const ProjectSchema = new mognoose.Schema({
+  title: { type: String, required: true },
+  position: { type: String, required: true, trim: true },
+  description: { type: String, default: '' },
+  link: { type: String, default: '', trime: true },
+  startTime: { type: Date, default: null },
+  endTime: { type: Date, default: null },
+}, {
+  id: true,
+  timestamps: true,
+  toObject: {
+    virtuals: true,
+  },
+  toJSON: {
+    virtuals: true,
+    versionKey: false,
+  },
+})
+
+/**
+ * Indexes
+ */
+ProjectSchema.index({ profile: 1 })
+
+module.exports = MongoConnection().model('projects', ProjectSchema)
