5913ed4c8e9f6655d50ab2649e73e8e78f5a9380
emanuelosva <emanuelosva@gmail.com>
2021-02-11 18:59:07 -0600

    chore(Fix): validator must return boolans

--- packages/common-module/src/validators/index.js
+++ packages/common-module/src/validators/index.js
@@ -1,11 +1,5 @@
 const { Types: { ObjectId } } = require('mongoose')
 
 module.exports = {
-  isMongoId: (id) => {
-    try {
-      ObjectId(id)
-    } catch (error) {
-      throw new Error('invalid id')
-    }
-  },
+  isMongoId: (id) => ObjectId.isValid(id),
 }
