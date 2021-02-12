6db69b7d688ee10bb4feeac93b78e4363b680ad2
emanuelosva <emanuelosva@gmail.com>
2021-02-11 18:51:27 -0600

    chore(Refactor): mkae available all error entities

--- packages/common-module/src/index.js
+++ packages/common-module/src/index.js
@@ -1,4 +1,4 @@
-const BuildBusinessError = require('./BussinesError')
+const { BuildBusinessError, toBusinessError } = require('./BussinesError')
 const makeGetEnvPrefix = require('./env')
 
 const BusinessError = new BuildBusinessError('common-module')
@@ -6,5 +6,7 @@ const BusinessError = new BuildBusinessError('common-module')
 module.exports.Logger = require('./logger')
 module.exports.constants = require('./constants')
 module.exports.slugify = require('./slugify')
+module.exports.validators = require('./validators')
+module.exports.toBusinessError = toBusinessError
 module.exports.BuildBusinessError = BuildBusinessError
 module.exports.getEnvPrefix = makeGetEnvPrefix({ BusinessError })
