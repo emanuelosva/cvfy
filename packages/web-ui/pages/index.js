89ce3f45105e0624c15cc26bed7d54c548ea5088
emanuelosva <emanuelosva@gmail.com>
2021-02-23 23:34:31 -0600

    chore(Setup): web ui project setup

---
+++ packages/web-ui/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Cvfy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Cvfy App</h1>
+    </div>
+  )
+}
