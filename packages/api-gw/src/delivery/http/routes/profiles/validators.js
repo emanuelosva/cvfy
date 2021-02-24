e26fa8ab479a0d3477fb36d3b9c4e7d82b19bb82
emanuelosva <emanuelosva@gmail.com>
2021-02-23 22:28:13 -0600

    chore(Fix): fix input validation for profile related entities

--- packages/api-gw/src/delivery/http/routes/profiles/validators.js
+++ packages/api-gw/src/delivery/http/routes/profiles/validators.js
@@ -1,20 +1,24 @@
 const Joi = require('joi')
 const { constants: { inputs, profiles } } = require('../../../../utils')
 
+const idSchema = Joi.string().regex(inputs.MONGO_ID_REGEX).message('invalid id')
+
 const idInParamsValidator = {
-  params: {
-    id: Joi.string().regex(inputs.MONGO_ID_REGEX).message('invalid id').required(),
-  },
+  params: Joi.object({
+    id: idSchema.required(),
+  }),
 }
 
 const slugInParamsValidator = {
-  params: {
+  params: Joi.object({
     slug: Joi.string().min(2).max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
-  },
+  }),
 }
 
 const updateProfile = {
-  ...idInParamsValidator,
+  params: Joi.object({
+    id: idSchema.required(),
+  }),
   body: Joi.object({
     template: Joi.string().valid(...Object.values(profiles.templateNames)),
     fullName: Joi.string().min(2).max(inputs.MAX_GENERAL_STRING_LENGTH),
@@ -37,94 +41,105 @@ const updateProfile = {
 }
 
 const createProfile = {
-  body: {
-    ...updateProfile.body,
+  body: Joi.object({
     template: Joi.string().valid(...Object.values(profiles.templateNames)).required(),
-    owner: Joi.string().max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
     fullName: Joi.string().min(2).max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
-  },
+    description: Joi.string().max(500),
+    contact: Joi.object({
+      email: Joi.string().email().max(inputs.MAX_GENERAL_STRING_LENGTH),
+      phoneNumber: Joi.string().regex(inputs.PHONE_NUMBER_REGEX),
+      optionalLink: Joi.string().uri(),
+    }),
+    socialLinks: Joi.array().optional().items(
+      Joi.object({
+        name: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
+        link: Joi.string().uri().max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
+      }),
+    ),
+    skills: Joi.array().optional().items(
+      Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
+    ),
+  }),
 }
 
 const updateProject = {
-  ...idInParamsValidator,
-  body: {
-    profileId: Joi.string().regex(inputs.MONGO_ID_REGEX).message('invalid id').required(),
-    project: Joi.object({
-      title: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
-      position: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
-      description: Joi.string().min(1).max(500),
-      link: Joi.string().uri().max(inputs.MAX_GENERAL_STRING_LENGTH),
-      startTime: Joi.date(),
-      endTimeTime: Joi.date(),
-    }),
-  },
+  params: Joi.object({
+    id: idSchema.required(),
+  }),
+  body: Joi.object({
+    title: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
+    position: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
+    description: Joi.string().min(1).max(500),
+    link: Joi.string().uri().max(inputs.MAX_GENERAL_STRING_LENGTH),
+    startTime: Joi.date(),
+    endTime: Joi.date(),
+  }),
 }
 
 const addProject = {
-  ...updateProject,
-  body: {
-    ...updateProject.body,
-    project: {
-      ...updateProject.body.project,
+  body: Joi.object({
+    profileId: Joi.string().regex(inputs.MONGO_ID_REGEX).message('invalid id').required(),
+    project: Joi.object({
       title: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
       position: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
-      description: Joi.string().min(1).max(500).required(),
-      link: Joi.string().uri().max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
-    },
-  },
+      description: Joi.string().min(1).max(500),
+      link: Joi.string().uri().max(inputs.MAX_GENERAL_STRING_LENGTH),
+      startTime: Joi.date(),
+      endTime: Joi.date(),
+    }),
+  }),
 }
 
 const updateJob = {
-  ...idInParamsValidator,
-  body: {
-    profileId: Joi.string().regex(inputs.MONGO_ID_REGEX).message('invalid id').required(),
-    job: Joi.object({
-      company: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
-      position: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
-      description: Joi.string().min(1).max(500),
-      startTime: Joi.date(),
-      endTimeTime: Joi.date(),
-    }),
-  },
+  params: Joi.object({
+    id: idSchema.required(),
+  }),
+  body: Joi.object({
+    company: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
+    position: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
+    description: Joi.string().min(1).max(500),
+    startTime: Joi.date(),
+    endTime: Joi.date(),
+  }),
 }
 
 const addJob = {
-  ...updateJob,
-  body: {
-    ...updateJob.body,
-    job: {
-      ...updateJob.body.job,
+  body: Joi.object({
+    profileId: Joi.string().regex(inputs.MONGO_ID_REGEX).message('invalid id').required(),
+    job: Joi.object({
       company: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
       position: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
-      description: Joi.string().min(1).max(500).required(),
-    },
-  },
+      description: Joi.string().min(1).max(500),
+      startTime: Joi.date(),
+      endTime: Joi.date(),
+    }),
+  }),
 }
 
 const updateEducation = {
-  ...idInParamsValidator,
-  body: {
-    profileId: Joi.string().regex(inputs.MONGO_ID_REGEX).message('invalid id').required(),
-    education: Joi.object({
-      title: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
-      school: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
-      certificateCode: Joi.string().min(1).max(500),
-      startTime: Joi.date(),
-      endTimeTime: Joi.date(),
-    }),
-  },
+  params: Joi.object({
+    id: idSchema.required(),
+  }),
+  body: Joi.object({
+    title: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
+    school: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH),
+    certificateCode: Joi.string().min(1).max(500),
+    startTime: Joi.date(),
+    endTime: Joi.date(),
+  }),
 }
 
 const addEducation = {
-  ...updateEducation,
-  body: {
-    ...updateEducation.body,
-    education: {
-      ...updateEducation.body.education,
+  body: Joi.object({
+    profileId: Joi.string().regex(inputs.MONGO_ID_REGEX).message('invalid id').required(),
+    education: Joi.object({
       title: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
       school: Joi.string().min(1).max(inputs.MAX_GENERAL_STRING_LENGTH).required(),
-    },
-  },
+      certificateCode: Joi.string().min(1).max(500),
+      startTime: Joi.date(),
+      endTime: Joi.date(),
+    }),
+  }),
 }
 
 module.exports = {
