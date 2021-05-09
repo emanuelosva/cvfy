405072a7af5f3093762fbefe249bff82c82750ee
emanuelosva <emanuelosva@gmail.com>
2021-05-09 16:06:51 -0500

    chore(NewFeature): common types

---
+++ packages/api-gw/src/delivery/graphql/commonTypes/index.js
@@ -0,0 +1,20 @@
+const {
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLScalarType,
+} = require('graphql')
+
+const VoidType = new GraphQLScalarType({
+  name: 'Void',
+  description: 'Represents NULL values',
+  serialize: () => null,
+  parseValue: () => null,
+  parseLiteral: () => null,
+})
+
+const NonNullableIDType = GraphQLNonNull(GraphQLID)
+
+module.exports = {
+  NonNullableIDType,
+  VoidType,
+}
