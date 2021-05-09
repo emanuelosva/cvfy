1f1a6b9793a60ce96fb0451937b45836ec223342
emanuelosva <emanuelosva@gmail.com>
2021-05-09 16:05:45 -0500

    chore(NewFeature): add graphql auth middleware

---
+++ packages/api-gw/src/delivery/graphql/middleware/auth.js
@@ -0,0 +1,47 @@
+const { GraphQLError } = require('graphql')
+const { ApiError } = require('../../../errors')
+
+const isAuthenticated = (ctx) => {
+  const { authError } = ctx
+  if (authError) {
+    throw new GraphQLError(authError.message)
+  }
+}
+
+const isAuthenticatedAndObjectOwner = (ctx, id) => {
+  const { user, authError } = ctx
+
+  if (authError) {
+    throw new GraphQLError(authError.message)
+  }
+
+  if (user.id !== id) {
+    try {
+      ApiError.throw(ApiError.types.FORBIDDEN)
+    } catch (error) {
+      throw new GraphQLError(authError.message)
+    }
+  }
+}
+
+const isAuthenticatedRole = async (ctx, ...roles) => {
+  const { user, authError } = ctx
+
+  if (authError) {
+    throw new GraphQLError(authError.message)
+  }
+
+  if (!roles.includes(user.type)) {
+    try {
+      ApiError.throw(ApiError.types.FORBIDDEN)
+    } catch (error) {
+      throw new GraphQLError(authError.message)
+    }
+  }
+}
+
+module.exports = {
+  isAuthenticated,
+  isAuthenticatedAndObjectOwner,
+  isAuthenticatedRole,
+}
